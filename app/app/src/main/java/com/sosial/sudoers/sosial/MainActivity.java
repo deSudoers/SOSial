ca8afc2cd851522e2daa6609a1998eff1dfab54b
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2018-10-17 20:36:26 +0530

    Add Trigger Activity

--- app/app/src/main/java/com/sosial/sudoers/sosial/MainActivity.java
+++ app/app/src/main/java/com/sosial/sudoers/sosial/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.InputStream;
@@ -68,6 +69,14 @@ public class MainActivity extends AppCompatActivity
         catch (Exception e){
             e.printStackTrace();
         }
+        Button openTrigger = (Button) findViewById(R.id.opentrigger);
+        openTrigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent(MainActivity.this, TriggerActivity.class);
+                startActivity(j);
+            }
+        });
     }
 
     class GetProfile extends AsyncTask<String, Void, String> {
